38e9b8777f21693ef28cfc3ab9f7d9c974787c02
zhaoxiyang <zhaoxiyang@neusoft.com>
2018-05-28 10:56:23 +0800

    创建组件

---
+++ src/views/home/Business.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    我是Business
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Business',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/home/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    我是Product
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
